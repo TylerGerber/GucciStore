bbf733618292d82b8abc10e72db75708994d4ac3
tyger <tyger@DESKTOP-5P2G9JP>
2017-10-14 10:08:38 -0400

    Gucci

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.slidesText = new System.Windows.Forms.TextBox();
+            this.beltText = new System.Windows.Forms.TextBox();
+            this.shirtText = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tenderText = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.calcualteChange = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.print = new System.Windows.Forms.Button();
+            this.subTotal = new System.Windows.Forms.Label();
+            this.tax = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            this.change = new System.Windows.Forms.Label();
+            this.newOrder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gucci Store";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Flip Flops";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Belt";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Shirt";
+            // 
+            // slidesText
+            // 
+            this.slidesText.Location = new System.Drawing.Point(85, 52);
+            this.slidesText.Name = "slidesText";
+            this.slidesText.Size = new System.Drawing.Size(55, 20);
+            this.slidesText.TabIndex = 4;
+            this.slidesText.Text = "0";
+            this.slidesText.TextChanged += new System.EventHandler(this.slidesText_TextChanged);
+            // 
+            // beltText
+            // 
+            this.beltText.Location = new System.Drawing.Point(85, 82);
+            this.beltText.Name = "beltText";
+            this.beltText.Size = new System.Drawing.Size(55, 20);
+            this.beltText.TabIndex = 5;
+            this.beltText.Text = "0";
+            // 
+            // shirtText
+            // 
+            this.shirtText.Location = new System.Drawing.Point(85, 116);
+            this.shirtText.Name = "shirtText";
+            this.shirtText.Size = new System.Drawing.Size(55, 20);
+            this.shirtText.TabIndex = 6;
+            this.shirtText.Text = "0";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(6, 142);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(134, 23);
+            this.Calculate.TabIndex = 7;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(2, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Sub Total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(45, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Tax";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(35, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Total";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Green;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label8.Location = new System.Drawing.Point(0, 265);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(140, 10);
+            this.label8.TabIndex = 11;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tenderText
+            // 
+            this.tenderText.Location = new System.Drawing.Point(85, 283);
+            this.tenderText.Name = "tenderText";
+            this.tenderText.Size = new System.Drawing.Size(55, 20);
+            this.tenderText.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(11, 283);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Tenderd";
+            // 
+            // calcualteChange
+            // 
+            this.calcualteChange.Location = new System.Drawing.Point(6, 309);
+            this.calcualteChange.Name = "calcualteChange";
+            this.calcualteChange.Size = new System.Drawing.Size(134, 23);
+            this.calcualteChange.TabIndex = 14;
+            this.calcualteChange.Text = "Calculate Change";
+            this.calcualteChange.UseVisualStyleBackColor = true;
+            this.calcualteChange.Click += new System.EventHandler(this.calcualteChange_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(14, 348);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 20);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Change";
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(6, 384);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(134, 23);
+            this.print.TabIndex = 16;
+            this.print.Text = "Print";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // subTotal
+            // 
+            this.subTotal.AutoSize = true;
+            this.subTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotal.Location = new System.Drawing.Point(106, 168);
+            this.subTotal.Name = "subTotal";
+            this.subTotal.Size = new System.Drawing.Size(0, 20);
+            this.subTotal.TabIndex = 17;
+            // 
+            // tax
+            // 
+            this.tax.AutoSize = true;
+            this.tax.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tax.Location = new System.Drawing.Point(106, 199);
+            this.tax.Name = "tax";
+            this.tax.Size = new System.Drawing.Size(0, 20);
+            this.tax.TabIndex = 18;
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total.Location = new System.Drawing.Point(106, 233);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(0, 20);
+            this.total.TabIndex = 19;
+            // 
+            // change
+            // 
+            this.change.AutoSize = true;
+            this.change.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.change.Location = new System.Drawing.Point(106, 348);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(0, 20);
+            this.change.TabIndex = 20;
+            // 
+            // newOrder
+            // 
+            this.newOrder.Location = new System.Drawing.Point(240, 384);
+            this.newOrder.Name = "newOrder";
+            this.newOrder.Size = new System.Drawing.Size(75, 23);
+            this.newOrder.TabIndex = 21;
+            this.newOrder.Text = "New Order";
+            this.newOrder.UseVisualStyleBackColor = true;
+            this.newOrder.UseWaitCursor = true;
+            this.newOrder.Click += new System.EventHandler(this.newOrder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(381, 419);
+            this.Controls.Add(this.newOrder);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.tax);
+            this.Controls.Add(this.subTotal);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.calcualteChange);
+            this.Controls.Add(this.tenderText);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.shirtText);
+            this.Controls.Add(this.beltText);
+            this.Controls.Add(this.slidesText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox slidesText;
+        private System.Windows.Forms.TextBox beltText;
+        private System.Windows.Forms.TextBox shirtText;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tenderText;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button calcualteChange;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.Label subTotal;
+        private System.Windows.Forms.Label tax;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label change;
+        private System.Windows.Forms.Button newOrder;
+    }
+}
+
---
+++ calculator/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//yo
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        //constants
+        const double FLIP_FLOPS = 250;
+        const double BELT = 900;
+        const double SHIRT = 700;
+        const double TAX = 1.13;
+        const string CLEAR = " ";
+
+        //variables
+        double numOfFlips = 0;
+        double numOFBelt = 0;
+        double numOfShirt = 0;
+        double numOfTax;
+        double numOfTendered;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+
+                numOfFlips = Convert.ToDouble(slidesText.Text);
+                numOFBelt = Convert.ToDouble(beltText.Text);
+                numOfShirt = Convert.ToDouble(shirtText.Text);
+               
+
+                double numOfSub = numOfFlips * FLIP_FLOPS
+                + numOFBelt * BELT
+                + numOfShirt * SHIRT;
+
+                numOfTax = TAX * numOfSub;                   
+
+                subTotal.Text = "$" + numOfSub;
+
+                tax.Text = "$" + Convert.ToString(numOfTax - numOfSub);
+                total.Text = "$" + numOfSub * TAX;
+            }
+            catch
+            {
+                subTotal.Text = "idiot";
+            }
+            
+            
+        }
+
+        private void slidesText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcualteChange_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+                numOfTendered = Convert.ToDouble(tenderText.Text);
+
+                change.Text = "$" + Convert.ToString(numOfTendered - numOfTax);
+            }
+           
+            catch
+            {
+                change.Text = "idiot";
+            }
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            double beltPrice = numOFBelt * BELT;
+            double shirtPrice = numOfShirt * SHIRT;
+            double flipPrice = numOfFlips + FLIP_FLOPS;
+
+            Graphics fg = this.CreateGraphics();
+            Font drawFont = new Font("Arial", 13, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            fg.Clear(Color.White);
+
+
+            fg.DrawString("Recipt" + "\n" + "September 28, 2008" + '\n' + '\n' + "Belt: x" 
+                + numOFBelt + " @ $" + beltPrice + "\n" + "Shirt: x" + shirtPrice + "\n"
+                + "Flip Flops: x" + flipPrice + '\n' + "\n" + "Subtotal: " + subTotal.Text
+                + '\n' + "Tax: " + tax.Text + "\n" + "Total: " + total.Text + "\n" + '\n'
+                + "Tenderd: $" + tenderText.Text + "\n" + "Change: " + change.Text 
+                + "\n" + "Have a nice day ;)", drawFont, drawBrush, 210, 50);
+
+           
+        }
+
+        private void newOrder_Click(object sender, EventArgs e)
+        {
+            Graphics fg = this.CreateGraphics();
+            fg.Clear(Color.White);
+
+            tax.Text = CLEAR; 
+            subTotal.Text = CLEAR;
+            total.Text = CLEAR;
+            change.Text = CLEAR;
+        }
+    }
+}
